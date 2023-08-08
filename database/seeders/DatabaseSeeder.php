bdb616df68b506ac044c372df09e4b62e77e11bf
agismandala007 <agixsatria@gmail.com>
2023-08-08 20:20:35 +0700

    re-upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,19 +22,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
         
         \App\Models\Cabang::factory()->create([
                 'name' => "Lazismu",
             ]);
-        // \App\Models\User::factory()->create([
-        //         'name' => 'Admin',
-        //         'email' => 'admin@lazismu.com',
-        //         'cabang_id' => 1,
-        //         'role' => 1,
-        //         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        \App\Models\User::factory()->create([
+                'name' => 'Admin',
+                'email' => 'admin@lazismu.com',
+                'cabang_id' => 1,
+                'role' => 1,
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 
-        //     ]);
+            ]);
         
 
         // Cabang::factory(5)->create();
